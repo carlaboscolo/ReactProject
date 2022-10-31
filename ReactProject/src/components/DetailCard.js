e42fbb66fd4c11df02515343cdb4009988bcabd3
Carla <100995659+carlaboscolo@users.noreply.github.com>
2022-10-31 21:00:12 +0100

    modify DetailCard.js

--- ReactProject/src/components/DetailCard.js
+++ ReactProject/src/components/DetailCard.js
@@ -1,17 +1,104 @@
 import React, { memo } from "react";
 import { View, StyleSheet, Text, Image, ScrollView } from "react-native";
 
-const DetailCard = memo(({ }) => {
+const DetailCard = memo(({ image, title , releaseDate, vote, description}) => {
 
-    
+    const imageUrl = "https://image.tmdb.org/t/p/w500/" + image;
+
     return(
-       <View> </View>
+        <ScrollView
+        contentContainerStyle={styles.scroll}
+        scrollEnabled={true}
+        showsVerticalScrollIndicator={false}
+        style={styles.container} >
+
+        <View style={styles.innerContainer}>
+           {/* immagine  */}
+           <View style={styles.containerImage}>
+              <Image source={{ uri: imageUrl }} style={styles.image} />
+           </View>
+           {/* testo  */}
+           <View style={styles.containerText}>
+              <View style={{ flex: 1 }}>
+
+                 <View style={styles.infoContainer}>
+                    <View style={styles.dateContainer}>
+
+                       <Text style={styles.date}> {releaseDate} </Text>
+                    </View>
+                    <View style={[styles.voteContainer, {
+                       backgroundColor: vote >= 6 ? 'rgb(101,224,136)' : 'rgb(253,90,37)',
+                    }]}>
+                       <Text style={styles.vote}> {vote} </Text>
+                    </View>
+                 </View>
+                 <Text style={styles.title}> {title} </Text>
+                 <Text style={styles.description}> {description} </Text>
+              </View>
+           </View>
+        </View>
+     </ScrollView>
     );
 
 });
 
 const styles = StyleSheet.create({
-   
+    container: {
+        backgroundColor: 'rgb(24,24,24)',
+     },
+    scroll: {
+        flex: 1,
+        backgroundColor: 'rgb(24,24,24)',
+        padding: 10,
+     },
+     innerContainer: {
+        borderRadius: 10,
+        height: '100%',
+        shadowColor: "#000",
+        shadowOffset: {
+           width: 0,
+           height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+  
+     },
+     containerImage: {
+        flex: 1,
+     },
+     image: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+     },
+     containerText: {
+        flexDirection: 'row',
+        padding: 10,
+     },
+     title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: 'white',
+     },
+     date: { fontSize: 15, color: 'white' },
+     vote: { fontSize: 16 },
+     description: { fontSize: 12, color: 'white', marginTop: 20, },
+     voteContainer: {
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        alignContent: 'center',
+        justifyContent: 'center'
+     },
+     infoContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+     },
+     dateContainer: {
+        flex: 1,
+     }
 });
 
 
--- ReactProject/src/screens/SerieDetail.js
+++ ReactProject/src/screens/SerieDetail.js
@@ -1,108 +1,22 @@
-import React, { useCallback, useState, useEffect } from "react";
-import { ScrollView, StyleSheet, Text, Image, View, FlatList } from "react-native";
+import React from "react";
+
+//component
+import DetailCard from "../components/DetailCard";
 
 const SerieDetail = ({ route, navigation }) => {
 
    const { data } = route.params;
-   const imageUrl = "https://image.tmdb.org/t/p/w500/" + data.poster_path;
-
-
 
    return (
-      <ScrollView
-         contentContainerStyle={styles.scroll}
-         scrollEnabled={true}
-         showsVerticalScrollIndicator={false}
-         style={styles.container} >
-
-         <View style={styles.innerContainer}>
-            {/* immagine  */}
-            <View style={styles.containerImage}>
-               <Image source={{ uri: imageUrl }} style={styles.image} />
-            </View>
-            {/* testo  */}
-            <View style={styles.containerText}>
-               <View style={{ flex: 1 }}>
-
-                  <View style={styles.infoContainer}>
-                     <View style={styles.dateContainer}>
-
-                        <Text style={styles.date}>{data.first_air_date} </Text>
-                     </View>
-                     <View style={[styles.voteContainer, {
-                        backgroundColor: data.vote_average >= 6 ? 'rgb(101,224,136)' : 'rgb(253,90,37)',
-                     }]}>
-                        <Text style={styles.vote}> {data.vote_average} </Text>
-                     </View>
-                  </View>
-                  <Text style={styles.title}>{data.original_name} </Text>
-                  <Text style={styles.description}>{data.overview} </Text>
-               </View>
-            </View>
-         </View>
-      </ScrollView>
+     <DetailCard
+     image={data.poster_path}
+     title={data.original_name}
+     releaseDate={data.first_air_date}
+     vote={data.vote_average}
+     description={data.overview}
+     />
    );
 };
-
-const styles = StyleSheet.create({
-   container: {
-      backgroundColor: 'rgb(24,24,24)',
-   },
-   scroll: {
-      flex: 1,
-      backgroundColor: 'rgb(24,24,24)',
-      padding: 10,
-   },
-
-   innerContainer: {
-      borderRadius: 10,
-      height: '100%',
-      shadowColor: "#000",
-      shadowOffset: {
-         width: 0,
-         height: 2,
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 3.84,
-      elevation: 5,
-
-   },
-   containerImage: {
-      flex: 1,
-   },
-   image: {
-      width: '100%',
-      height: '100%',
-      resizeMode: 'cover',
-   },
-   containerText: {
-      flexDirection: 'row',
-      padding: 10,
-   },
-   title: {
-      fontSize: 30,
-      fontWeight: 'bold',
-      color: 'white',
-   },
-   date: { fontSize: 15, color: 'white' },
-   vote: { fontSize: 16 },
-   description: { fontSize: 12, color: 'white', marginTop: 20, },
-   voteContainer: {
-      width: 30,
-      height: 30,
-      borderRadius: 15,
-      alignContent: 'center',
-      justifyContent: 'center'
-   },
-   infoContainer: {
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-   },
-   dateContainer: {
-      flex: 1,
-   }
-});
 
 export default SerieDetail;
 
