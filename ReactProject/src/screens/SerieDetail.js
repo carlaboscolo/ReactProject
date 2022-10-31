14dbc3788284bf55d4303b70ce4609fe94a0c50a
Carla <100995659+carlaboscolo@users.noreply.github.com>
2022-10-31 16:48:43 +0100

    Passing params in Serie Detail

--- ReactProject/src/screens/SerieDetail.js
+++ ReactProject/src/screens/SerieDetail.js
@@ -1,21 +1,37 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, Text, Image } from "react-native";
+import { View } from "react-native-web";
 
 
-const SerieDetail = () => {
+const SerieDetail = ({ route, navigation }) => {
+
+   const { data } = route.params;
+   const imageUrl = "https://image.tmdb.org/t/p/w500/" + data.poster_path ;
+
 return(
-  <View style = {styles.container}>
-  </View>
+   <ScrollView
+   contentContainerStyle={styles.scroll}
+   scrollEnabled={false}
+   showsVerticalScrollIndicator={false}
+>
+ 
+   <Image source={{ uri: imageUrl }}  style={styles.image}/>
+   <Text> {data.original_name} </Text> 
+  
+  </ScrollView>
 );
 };
 
 const styles = StyleSheet.create({
-  container : {
-      flex: 1,
-      backgroundColor: 'rgb(24,24,24)',
-      alignItems : 'center',
-      justifyContent : 'center',
-  },
+  image: {
+   width: '100%',
+   height: '100%',
+   resizeMode: 'contain',
+},
+scroll: {
+   flexGrow: 1,
+   backgroundColor: 'rgb(24,24,24)',
+},
 });
 
 export default SerieDetail;
