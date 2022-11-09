20528b3343ccd391804afeda9d5e2681957f5f49
Carla <100995659+carlaboscolo@users.noreply.github.com>
2022-11-09 21:05:13 +0100

    stack detail user

--- ReactProject/src/navigation/UserStack.js
+++ ReactProject/src/navigation/UserStack.js
@@ -3,6 +3,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 //screens
 import UserPage from "../screens/UserPage";
+import MovieDetail from "../screens/MovieDetail";
+import SerieDetail from "../screens/SerieDetail";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +16,34 @@ const UserStack = () => {
         component={UserPage}
         options={{
           title: 'Profilo',
+          headerStyle: {
+            backgroundColor: 'rgb(80, 54, 97)',
+          },
+          headerTintColor: 'white',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
+       <Stack.Screen
+        name="MovieDetailSearch"
+        component={MovieDetail}
+        options={{
+          title: 'Dettaglio film',
+          headerStyle: {
+            backgroundColor: 'rgb(80, 54, 97)',
+          },
+          headerTintColor: 'white',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
+      <Stack.Screen
+        name="SerieDetailSearch"
+        component={SerieDetail}
+        options={{
+          title: 'Dettaglio Serie',
           headerStyle: {
             backgroundColor: 'rgb(80, 54, 97)',
           },
